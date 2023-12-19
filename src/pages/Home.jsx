d734b65bbc047015d8239c722a8b38c313f30b9a
Ritty Reach <ritty690@gmail.com>
2023-12-19 23:21:24 +0700

    edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const datas = [
     id: 1,
     greeting: "Hello, My name's",
     name: "Rithyreach",
-    detail: "I'm a student of department computer science, years there in the Royal University of Phnom Penh.",
+    detail: "I'm a student of department computer science, years three in the Royal University of Phnom Penh.",
     job: "Front-end developer",
     img: pic,
   },
